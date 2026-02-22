777513b3ee8db87353582f988e7bcfed7512a8c8
Andras Cseke <csekebandi@gmail.com>
2026-02-22 19:31:47 +0100

    SQL szerver TEST iNsTANCE probalkozas

--- berles2/DatabaseConfig.cs
+++ berles2/DatabaseConfig.cs
@@ -3,6 +3,6 @@
     public static class DatabaseConfig
     {
         public static string ConnectionString =>
-            @"Server=192.168.68.241,1433;Database=ToolRentalDB;User Id=toolrentaluser;Password=ToolRental2025!;TrustServerCertificate=True;";
+            @"Server=192.168.68.241,1433;Database=ToolRentalDB_TEST;User Id=toolrentaluser;Password=ToolRental2025!;TrustServerCertificate=True;";
     }
 }
